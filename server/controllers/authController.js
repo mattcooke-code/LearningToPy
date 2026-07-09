da0fa475a5763c6c8130e7dce5ddde2da3dad5b2
Matt <mattcooke200@hotmail.com>
2026-07-09 09:18:35 +0100

    Upgrading auth of existing accounts to pepper: restoring pepper to login and delete

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -218,9 +218,9 @@ const login = catchAsync(async (req, res, next) => {
     );
   }
 
-  //const peppered = applyPepper(password);
+  const peppered = applyPepper(password);
 
-  const isMatch = await bcrypt.compare(password, user.password);
+  const isMatch = await bcrypt.compare(peppered, user.password);
   if (!isMatch) {
     return next(
       new AppError("Invalid credentials or account is blocked.", 401),
@@ -666,8 +666,8 @@ const deleteAccount = catchAsync(async (req, res, next) => {
     return next(new AppError("User not found.", 404));
   }
 
-  //const peppered = applyPepper(password);
-  const isMatch = await bcrypt.compare(password, user.password);
+  const peppered = applyPepper(password);
+  const isMatch = await bcrypt.compare(peppered, user.password);
   if (!isMatch) {
     return next(
       new AppError("Password is incorrect. Account not deleted.", 401),
